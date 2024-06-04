237ff2551ddad9463592cf9dd5a52413a7dbe9fa
amirrezaask <raskarpour@gmail.com>
2024-06-05 03:18:09 +0330

    Fix columnar view example

--- example/columnar_view/main.go
+++ example/columnar_view/main.go
@@ -11,11 +11,8 @@ func main() {
 	gpui.New(func(g *gpui.GPUI, windowCtx gpui.WindowContext, frameEvents []gpui.Event) {
 		columns := gpui.ColumnarAreas(windowCtx.Area(), 3)
 		for i, col := range columns {
-			listAreas := gpui.ListAreas(col, 3)
-			for j, lis := range listAreas {
-				button := &gpui.Button{FillColor: rl.Red, Label: "Button " + fmt.Sprint(i*j)}
-				button.Render(g, lis, frameEvents)
-			}
+			button := &gpui.Button{FillColor: rl.Red, Label: "Button " + fmt.Sprint(i)}
+			button.Render(g, col, frameEvents)
 		}
 	}).
 		WithFont("LiberationMono.ttf").
